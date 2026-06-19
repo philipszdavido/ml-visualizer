49414cf35d3cd90b59e4a9dafd81c227a6578b3c
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-19 13:11:18 +0100

    feat: refactored to new ng features

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -14,7 +14,7 @@ export default {
   assets: {
     'index.csr.html': {size: 502, hash: '340085c0cae46b6320473c5c92081b1a918ad971d893dc028a4d4d250699fc03', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
     'index.server.html': {size: 1015, hash: 'f2bd074a3e127608733cdf48284fca8c30431e6fd279f9c869c9721c0c56b17e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 8353, hash: '121a2f87c52aa0b9e1502fceeca0810bc08be86a968595a965d004fe2f071883', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.html': {size: 8352, hash: '9dc5d3014e6686369bce12e5ced5772178c6e5cc03050f440cd2b3528fbcb060', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-5INURTSO.css': {size: 0, hash: 'menYUTfbRu8', text: () => import('./assets-chunks/styles-5INURTSO_css.mjs').then(m => m.default)}
   },
 };
