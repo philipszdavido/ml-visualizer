2756c2fc6ccde8ce9bdb745dc8a19767e1241d67
Chidume Nnamdi <kurtwanger40@gmail.com>
2026-06-19 22:05:53 +0100

    feat: moved code to different components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { MlSandbox } from './ml-sandbox/ml-sandbox';
+import { EmbeddingSimComponent } from './embedding-sim/embedding-sim';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'ml-sandbox',
+    component: MlSandbox
+  },
+  {
+    path: 'embed-sim',
+    component: EmbeddingSimComponent
+  }
+];
